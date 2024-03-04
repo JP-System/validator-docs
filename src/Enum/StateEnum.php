f4910d6f06d27be89df5fd9102f35898b61735b4
João Pedro Lopes <joaopalopes24@gmail.com>
2024-03-04 11:59:02 -0300

    feat(state): adding label in state enum

--- src/Enum/StateEnum.php
+++ src/Enum/StateEnum.php
@@ -32,4 +32,38 @@ enum StateEnum: string
     case SE = 'SE';
     case TO = 'TO';
     case EX = 'EX';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::AC => 'Acre',
+            self::AL => 'Alagoas',
+            self::AP => 'Amapá',
+            self::AM => 'Amazonas',
+            self::BA => 'Bahia',
+            self::CE => 'Ceará',
+            self::DF => 'Distrito Federal',
+            self::ES => 'Espírito Santo',
+            self::GO => 'Goiás',
+            self::MA => 'Maranhão',
+            self::MT => 'Mato Grosso',
+            self::MS => 'Mato Grosso do Sul',
+            self::MG => 'Minas Gerais',
+            self::PA => 'Pará',
+            self::PB => 'Paraíba',
+            self::PR => 'Paraná',
+            self::PE => 'Pernambuco',
+            self::PI => 'Piauí',
+            self::RJ => 'Rio de Janeiro',
+            self::RN => 'Rio Grande do Norte',
+            self::RS => 'Rio Grande do Sul',
+            self::RO => 'Rondônia',
+            self::RR => 'Roraima',
+            self::SC => 'Santa Catarina',
+            self::SP => 'São Paulo',
+            self::SE => 'Sergipe',
+            self::TO => 'Tocantins',
+            self::EX => 'Exterior',
+        };
+    }
 }
